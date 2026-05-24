3bd735a821ddb2ba932eea03cf9959d5574fd8e3
purnakoppadi <purnakoppadi@gmail.com>
2026-05-24 15:51:31 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ app.use(
       return callback(new Error('Not allowed by CORS'));
     },
 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 
     credentials: true,
   })
